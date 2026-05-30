14bb71f7cfecb473a2835212c4e39ade7916e987
nectarboy <mushrombrother@gmail.com>
2026-05-30 15:05:46 -0400

    feat: create global state that will hold all app's backend state

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import { createServer } from "node:http";
+import next from "next";
+import { State } from "./state";
+
+declare global {
+    var state: State;
+}
+
+function initState() {
+    global.state = new State();
+}
+
+export function getState() {
+    return global.state;
+}
+
+process.env.NEXT_RUNTIME = 'nodejs';
+
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
+
+app.prepare().then(() => {
+    const handler = app.getRequestHandler();
+    const server = createServer(async (req, res) => {
+        await handler(req, res);
+    });
+
+    initState();
+
+    server.listen(3000, () => {
+        console.log('Server running on port 3000.');
+    });
+});
